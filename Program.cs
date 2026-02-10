a3e24b9d77e15986866a45faa0248605e05880be
Anton <erikssonanton94@gmail.com>
2026-02-10 13:35:16 +0100

    Anslutningskod skapad i program.cs och appsettings.json

--- Program.cs
+++ Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using React2.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<TodoDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultDbString"))
+);
 
 var app = builder.Build();
 
